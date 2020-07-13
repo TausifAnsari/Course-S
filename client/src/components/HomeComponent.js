fe26af0ab913983d8587ff95a21fb6a196723ee6
shaik afroz <shaikafroz0016@gmail.com>
2020-07-14 01:03:51 +0530

    changes

--- client/src/components/HomeComponent.js
+++ client/src/components/HomeComponent.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Card, CardImg,  CardBody, CardTitle,Button,CardSubtitle,CardText } from 'reactstrap';
-import UncontrolledCollapse from 'reactstrap/lib/UncontrolledCollapse';
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
 import { Link } from 'react-router-dom';
@@ -23,20 +22,14 @@ function RenderCard({item, isLoading, errMess}) {
                 transformProps={{
                     exitTransform: 'scale(0.5) translateY(-50%)'
                 }}>
-                <Card className="shadow-lg p-3 mb-5 bg-white rounded">
+                <Card>
+                <Link to={`/Courses/${item._id}`} >
                     <CardImg src={baseUrl + item.image} alt={item.name} />
+                    </Link>
                     <CardBody>
                         <CardTitle>{item.name}</CardTitle>
-                        <div>
-                        <Button color="link" id="toggler" size="sm" style={{ marginBottom: '0rem' }}>
-                        Show Details
-                        </Button>
-                        <UncontrolledCollapse toggler="#toggler">
                         {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
                         <CardText>{item.description}</CardText>
-                        </UncontrolledCollapse>
-                    </div>
-                    <Link to={`/Courses/${item._id}`} > <Button color="primary">Go To Course</Button></Link>
                     </CardBody>
                 </Card>
             </FadeTransform>
@@ -60,56 +53,14 @@ function RenderCard1({item, isLoading, errMess}) {
                 transformProps={{
                     exitTransform: 'scale(0.5) translateY(-50%)'
                 }}>
-                <Card className="shadow-lg p-3 mb-5 bg-white rounded">
+                <Card>
+                <Link to={`/Courses/`} >
                     <CardImg src={baseUrl + item.image} alt={item.name} />
+                    </Link>
                     <CardBody>
                         <CardTitle>{item.name}</CardTitle>
-                        <div>
-                        <Button color="link" id="toggler1" size="sm" style={{ marginBottom: '0rem' }}>
-                        Show Details
-                        </Button>
-                        <UncontrolledCollapse toggler="#toggler1">
                         {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
                         <CardText>{item.description}</CardText>
-                        </UncontrolledCollapse>
-                    </div>
-                    <Link to={`/Courses/`} > <Button color="primary">Go To Course</Button></Link>
-                    </CardBody>
-                </Card>
-            </FadeTransform>
-        );
-}
-function RenderCard2({item, isLoading, errMess}) {
-    if (isLoading) {
-        return(
-            <Loading />
-        );
-    }
-    else if (errMess) {
-        return(
-            <h4>{errMess}</h4>
-        );
-    }
-    else
-        return(
-            <FadeTransform in 
-                transformProps={{
-                    exitTransform: 'scale(0.5) translateY(-50%)'
-                }}>
-                <Card className="shadow-lg p-3 mb-5 bg-white rounded">
-                    <CardImg src={baseUrl + item.image} alt={item.name} />
-                    <CardBody>
-                        <CardTitle>{item.name}</CardTitle>
-                        <div>
-                        <Button color="link" id="toggler2" size="sm" style={{ marginBottom: '0rem' }}>
-                        Show Details
-                        </Button>
-                        <UncontrolledCollapse toggler="#toggler2">
-                        {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
-                        <CardText>{item.description}</CardText>
-                        </UncontrolledCollapse>
-                    </div>
-                    <Link to={`/Courses/`} > <Button color="primary">Go To Course</Button></Link>
                     </CardBody>
                 </Card>
             </FadeTransform>
@@ -131,7 +82,7 @@ function Home(props) {
                         errMess={props.promosErrMess} />
                 </div>
                 <div className="col-12 col-md m-1">
-                    <RenderCard2 item={props.leader} 
+                    <RenderCard1 item={props.leader} 
                         isLoading={props.leaderLoading} 
                         errMess={props.leaderErrMess} />
                 </div>

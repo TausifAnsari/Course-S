bcaa6fb28069017608e47d86cef4ab741db7a33f
shaik afroz <shaikafroz0016@gmail.com>
2020-07-14 01:41:05 +0530

    changes

--- client/src/components/HomeComponent.js
+++ client/src/components/HomeComponent.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle,Button } from 'reactstrap';
+import { Card, CardImg, CardText,  CardTitle, CardSubtitle,Button } from 'reactstrap';
 import UncontrolledCollapse from 'reactstrap/lib/UncontrolledCollapse.js';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
 import { Link } from 'react-router-dom';
 import { FadeTransform } from 'react-animation-components';
+
+const ColoredLine = ({ color }) => (
+    <hr
+        style={{
+            color: color,
+            backgroundColor: color,
+            height: 0.1
+        }}
+    />
+);
 
 function RenderCard({item, isLoading, errMess}) {
     if (isLoading) {
@@ -23,29 +33,22 @@ function RenderCard({item, isLoading, errMess}) {
                 transformProps={{
                     exitTransform: 'scale(0.5) translateY(-50%)'
                 }}>
-                <Card>
-                <Link to={`/Courses/${item._id}`} >
+                <Card className="shadow-lg p-3 mb-5 bg-white rounded">
                     <CardImg src={baseUrl + item.image} alt={item.name} />
-                    </Link>
-                    <CardBody>
                         <CardTitle>{item.name}</CardTitle>
-                        {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
-                        <CardText>{item.description}</CardText>
-                    </CardBody>
                     <div>
-    <Button color="primary" id="toggler" style={{ marginBottom: '1rem' }}>
-      Toggle
-    </Button>
-    <UncontrolledCollapse toggler="#toggler">
-      <Card>
-        <CardBody>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt magni, voluptas debitis
-          similique porro a molestias consequuntur earum odio officiis natus, amet hic, iste sed
-          dignissimos esse fuga! Minus, alias.
-        </CardBody>
-      </Card>
-    </UncontrolledCollapse>
-  </div>
+                    <Button color="link" id="toggler" style={{ marginBottom: '1rem' }}>
+                    See details
+                    </Button>
+                    <UncontrolledCollapse toggler="#toggler">
+                    {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
+                    <CardText>{item.description}</CardText>
+                    </UncontrolledCollapse>
+                </div>
+                <ColoredLine color="black" />
+                <Link to={`/Courses/${item._id}`} >
+                <Button outline color="primary">Go To Course</Button>
+                </Link>
                 </Card>
             </FadeTransform>
         );
@@ -68,15 +71,60 @@ function RenderCard1({item, isLoading, errMess}) {
                 transformProps={{
                     exitTransform: 'scale(0.5) translateY(-50%)'
                 }}>
-                <Card>
-                <Link to={`/Courses/`} >
+                <Card className="shadow-lg p-3 mb-5 bg-white rounded">
                     <CardImg src={baseUrl + item.image} alt={item.name} />
-                    </Link>
-                    <CardBody>
                         <CardTitle>{item.name}</CardTitle>
-                        {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
-                        <CardText>{item.description}</CardText>
-                    </CardBody>
+                    <div>
+                    <Button color="link" id="toggler1" style={{ marginBottom: '1rem' }}>
+                    See details
+                    </Button>
+                    <UncontrolledCollapse toggler="#toggler1">
+                    {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
+                    <CardText>{item.description}</CardText>
+                    </UncontrolledCollapse>
+                </div>
+                <ColoredLine color="black" />
+                <Link to={`/Courses/`} >
+                <Button outline color="primary">Go To Course</Button>
+                </Link>
+                </Card>
+            </FadeTransform>
+        );
+}
+
+function RenderCard2({item, isLoading, errMess}) {
+    if (isLoading) {
+        return(
+            <Loading />
+        );
+    }
+    else if (errMess) {
+        return(
+            <h4>{errMess}</h4>
+        );
+    }
+    else
+        return(
+            <FadeTransform in 
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
+                <Card className="shadow-lg p-3 mb-5 bg-white rounded">
+                    <CardImg src={baseUrl + item.image} alt={item.name} />
+                        <CardTitle>{item.name}</CardTitle>
+                    <div>
+                    <Button color="link" id="toggler2" style={{ marginBottom: '1rem' }}>
+                    See details
+                    </Button>
+                    <UncontrolledCollapse toggler="#toggler2">
+                    {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
+                    <CardText>{item.description}</CardText>
+                    </UncontrolledCollapse>
+                </div>
+                <ColoredLine color="black" />
+                <Link to={`/Courses/`} >
+                <Button outline color="primary">Go To Course</Button>
+                </Link>
                 </Card>
             </FadeTransform>
         );
@@ -97,7 +145,7 @@ function Home(props) {
                         errMess={props.promosErrMess} />
                 </div>
                 <div className="col-12 col-md m-1">
-                    <RenderCard1 item={props.leader} 
+                    <RenderCard2 item={props.leader} 
                         isLoading={props.leaderLoading} 
                         errMess={props.leaderErrMess} />
                 </div>
